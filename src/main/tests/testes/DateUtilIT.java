4e89fe587cbe3ea9b1071f1071813c91ccc5f1e0
cauaKilou <caua.oliversouza@gmail.com>
2025-08-08 09:46:08 -0300

    Testes unitarios implementados

---
+++ src/main/tests/testes/DateUtilIT.java
@@ -0,0 +1,27 @@
+package testes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static br.ce.wcaquino.taskbackend.utils.DateUtils.isEqualOrFutureDate;
+
+class DateUtilIT {
+
+    @Test
+    void deveRetornarTrueParaFuturas(){
+        LocalDate date = LocalDate.of(2030,1,1);
+        Assertions.assertTrue(isEqualOrFutureDate(date));
+    }
+    @Test
+    void deveRetornarFalseParaDatasPassadas(){
+        LocalDate date = LocalDate.of(2010,1,1);
+        Assertions.assertFalse(isEqualOrFutureDate(date));
+    }
+    @Test
+    void deveRetornarTrueParaHoje(){
+        LocalDate date = LocalDate.now();
+        Assertions.assertTrue(isEqualOrFutureDate(date));
+    }
+
+
+}
